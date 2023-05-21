b5ad37e1b66f51de29b904de44a4435c18ade2fc
Nandan Bhowmick <nandanbhow123@gmail.com>
2023-05-21 16:11:57 +0600

    Create DFS.cpp

---
+++ DFS.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+
+using namespace std;
+
+int adj_mat[100][100];
+int color[100],previous[100],d[100],f[100];
+int t=0;
+
+void graph(int *num,int v1,int v2,int n)
+{
+    int i,j;
+    for(i=0;i<n;i++)
+        if(num[i]==v1)break;
+    for(j=0;j<n;j++)
+        if(num[j]==v2)break;
+    adj_mat[i][j]=1;
+    adj_mat[j][i]=1;
+}
+
+void dfsVisit(int *num,int n,int i)
+{
+    color[i]=1;
+    t++;
+    d[i]=t;
+    cout<<num[i]<<" ";
+    int j;
+    for(j=0;j<n;j++){
+        if(adj_mat[i][j]==1 && color[j]==0){
+            previous[j]=i;
+            dfsVisit(num,n,j);
+        }
+    }
+    color[i]=2;
+    t++;
+    f[i]=t;
+}
+
+void dfs(int *num,int n,int i)
+{
+    color[100]={0};
+    d[100]={10000000};
+    f[100]={10000000};
+    previous[100]={-1};
+
+    dfsVisit(num,n,i);
+    for(i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(adj_mat[i][j]==1 && color[j]==0)
+            dfsVisit(num,n,i);
+        }
+    }
+}
+
+int main()
+{
+    int n,x,i,v1,v2,j,temp;
+    cout<<"Enter number of vertices: ";
+    cin>>n;
+    cout<<"Enter number of edges: ";
+    cin>>x;
+    int num[n];
+    cout<<"Enter info of each vertex: "<<endl;
+    for(i=0;i<n;i++)
+        cin>>num[i];
+
+    cout<<"Enter the connections between vertices: "<<endl;
+    for(i=0;i<x;i++){
+        cin>>v1>>v2;
+        graph(num,v1,v2,n);
+    }
+
+
+    int source;
+    cout<<"Enter source vertex: ";
+    cin>>source;
+    for(i=0;i<n;i++)
+        if(num[i]==source)break;
+    dfs(num,n,i);
+
+    int f_sort[n];
+    for(i=0;i<n;i++){
+        f_sort[i]=num[i];
+    }
+
+    for(i=0;i<n-1;i++){
+        for(j=0;j<n-1;j++){
+            if(f[j+1]>f[j]){
+                temp=f_sort[j];
+                f_sort[j]=f_sort[j+1];
+                f_sort[j+1]=temp;
+
+                temp=f[j];
+                f[j]=f[j+1];
+                f[j+1]=temp;
+            }
+        }
+    }
+    cout<<endl;
+
+    for(i=0;i<n;i++)
+        cout<<f_sort[i]<<" ";
+
+
+    return 0;
+}
